6cbb64930340e5796157093487a5ffded1e5f171
Lucian Ghimpu <ghimpulucianeduard@gmail.com>
2020-09-16 00:33:15 +0300

    About Us page

---
+++ app/src/main/java/com/lucianghimpu/matchmefy/presentation/settings/AboutFragment.kt
@@ -0,0 +1,57 @@
+package com.lucianghimpu.matchmefy.presentation.settings
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import com.lucianghimpu.matchmefy.R
+import com.lucianghimpu.matchmefy.appServices.AppAnalytics
+import com.lucianghimpu.matchmefy.databinding.FragmentAboutBinding
+import com.lucianghimpu.matchmefy.presentation.BaseFragment
+import com.lucianghimpu.matchmefy.utilities.EventObserver
+import org.koin.android.viewmodel.ext.android.sharedViewModel
+
+
+class AboutFragment : BaseFragment<SettingsViewModel, FragmentAboutBinding>(
+    enterTransitionResId = android.R.transition.slide_right,
+    exitTransitionResId = android.R.transition.fade
+) {
+    override val viewModel: SettingsViewModel by sharedViewModel(from = { parentFragment!! })
+    override fun getLayoutResId(): Int = R.layout.fragment_about
+    override fun setViewDataBindingViewModel() { binding.viewModel = viewModel }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mainActivity.setBottomNavigationBarVisibility(View.VISIBLE)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initObservers()
+    }
+
+    private fun initObservers() {
+        viewModel.openLinkInBrowserEvent.observe(this@AboutFragment, EventObserver {
+            try {
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(it))
+                startActivity(browserIntent)
+                AppAnalytics.trackLog("Opened URL $it in browser")
+            } catch (ex: Exception) {
+                AppAnalytics.trackError(ex, "Failed to open URL $it in browser")
+            }
+        })
+
+        viewModel.openEmailEvent.observe(this@AboutFragment, EventObserver {
+            try {
+                val emailIntent = Intent(Intent.ACTION_SEND)
+
+                emailIntent.type = "plain/text"
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, listOf("matchmefy@gmail.com").toTypedArray())
+
+                startActivity(Intent.createChooser(emailIntent, "Contact us via Email"))
+            } catch (ex: Exception) {
+                AppAnalytics.trackError(ex, "Failed to open email")
+            }
+        })
+    }
+}
--- app/src/main/java/com/lucianghimpu/matchmefy/presentation/settings/SettingsViewModel.kt
+++ app/src/main/java/com/lucianghimpu/matchmefy/presentation/settings/SettingsViewModel.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.lucianghimpu.matchmefy.BuildConfig
 import com.lucianghimpu.matchmefy.MatchmefyApp
 import com.lucianghimpu.matchmefy.R
 import com.lucianghimpu.matchmefy.appServices.AppAnalytics
@@ -16,6 +17,7 @@ import com.lucianghimpu.matchmefy.presentation.dialogs.doubleButton.DoubleButton
 import com.lucianghimpu.matchmefy.presentation.dialogs.doubleButton.DoubleButtonDialogListener
 import com.lucianghimpu.matchmefy.presentation.dialogs.loading.LoadingDialog
 import com.lucianghimpu.matchmefy.utilities.ColoredTextSpan
+import com.lucianghimpu.matchmefy.utilities.Event
 import com.lucianghimpu.matchmefy.utilities.PreferencesConstants
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -34,11 +36,73 @@ class SettingsViewModel(
     val user: LiveData<User>
         get() = _user
 
+    private var _appVersion = MutableLiveData<String>()
+    val appVersion: LiveData<String>
+        get() = _appVersion
+
+    private var _openLinkInBrowserEvent = MutableLiveData<Event<String>>()
+    val openLinkInBrowserEvent: MutableLiveData<Event<String>>
+        get() = _openLinkInBrowserEvent
+
+    private var _openEmailEvent = MutableLiveData<Event<Any>>()
+    val openEmailEvent: MutableLiveData<Event<Any>>
+        get() = _openEmailEvent
+
     init {
         _user.value = preferencesService.getObject(
             PreferencesConstants.USER_PROFILE_KEY,
             User::class
         )
+
+        _appVersion.value = "AppVersion: ${BuildConfig.VERSION_NAME}"
+    }
+
+    fun onAboutClicked() {
+        AppAnalytics.trackEvent("about_clicked")
+        navigate(SettingsFragmentDirections.actionSettingsFragmentToAboutFragment())
+    }
+
+    fun openGitHubPage() {
+        AppAnalytics.trackEvent("github_clicked")
+        _openLinkInBrowserEvent.value = Event("https://github.com/GhimpuLucianEduard")
+    }
+
+    fun openWebsite() {
+        AppAnalytics.trackEvent("website_clicked")
+        _openLinkInBrowserEvent.value = Event("https://matchmefy.com")
+    }
+
+    fun openEmail() {
+        AppAnalytics.trackEvent("email_clicked")
+        _openEmailEvent.value = Event(Any())
+    }
+
+    fun onSignOutClicked() {
+        AppAnalytics.trackEvent("sign_out_clicked")
+        val signOutDialog = DoubleButtonDialog(
+            title = context.getString(R.string.sign_out_dialog_title),
+            description = context.getString(R.string.sign_out_dialog_description),
+            descriptionSpan = ColoredTextSpan(
+                50,
+                53
+            ),
+            positiveButtonText = context.getString(R.string.sign_out_dialog_button),
+            negativeButtonText = context.getString(R.string.cancel_dialog_button),
+            imageId = R.drawable.dialog_warning,
+            listener = object : DoubleButtonDialogListener {
+                override fun onPositiveButtonClicked() {
+                    AppAnalytics.trackEvent("sign_out")
+                    hideDialog()
+                    signOut()
+                }
+
+                override fun onNegativeButtonClicked() {
+                    AppAnalytics.trackEvent("cancel_sign_out")
+                    hideDialog()
+                }
+            }
+        )
+        showDialog(signOutDialog)
     }
 
     fun onDeleteDataClicked() {
@@ -68,34 +132,6 @@ class SettingsViewModel(
             }
         )
         showDialog(deleteUserDataDialog)
-    }
-
-    fun onSignOutClicked() {
-        AppAnalytics.trackEvent("sign_out_clicked")
-        val signOutDialog = DoubleButtonDialog(
-            title = context.getString(R.string.sign_out_dialog_title),
-            description = context.getString(R.string.sign_out_dialog_description),
-            descriptionSpan = ColoredTextSpan(
-                50,
-                53
-            ),
-            positiveButtonText = context.getString(R.string.sign_out_dialog_button),
-            negativeButtonText = context.getString(R.string.cancel_dialog_button),
-            imageId = R.drawable.dialog_warning,
-            listener = object : DoubleButtonDialogListener {
-                override fun onPositiveButtonClicked() {
-                    AppAnalytics.trackEvent("sign_out")
-                    hideDialog()
-                    signOut()
-                }
-
-                override fun onNegativeButtonClicked() {
-                    AppAnalytics.trackEvent("cancel_sign_out")
-                    hideDialog()
-                }
-            }
-        )
-        showDialog(signOutDialog)
     }
 
     private fun deleteUserData() {
